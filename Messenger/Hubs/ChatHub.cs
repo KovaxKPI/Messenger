c5834eaa89b0a318aab989b9b3288e8c123edffb
Max <maksymus423@gmail.com>
2022-08-20 15:25:11 +0300

    Implemented group, private and to all messages

--- Messenger/Hubs/ChatHub.cs
+++ Messenger/Hubs/ChatHub.cs
@@ -1,57 +1,32 @@
 ﻿using Messenger.Database;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Messenger.Hubs
 {
     public class ChatHub : Hub
     {
-        /*public async Task SendMessage(string user, string message)
-        {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
-        }*/
         private readonly ApplicationContext db;
         public ChatHub(ApplicationContext context)
         {
             db = context;
         }
-        /*public async Task SendMessageGroup(string user, string message, string roomName)
-        {
-            await Clients.Group(roomName).SendAsync("ReceiveMessage", user, message);
-        }
-        public async Task SendPrivateMessage(string user, string message, string to)
-        {
-            if (Context.UserIdentifier != to)
-                await Clients.User(Context.UserIdentifier).SendAsync("ReceiveMessage", user, message);
-            await Clients.User(to).SendAsync("ReceiveMessage", user, message);
-        }*/
+        
         public async Task SendMessage(string user, string message, string to, string roomName)
         {
-            if (!string.IsNullOrEmpty(to))
+            if(!string.IsNullOrEmpty(to))
             {
                 if (Context.UserIdentifier != to)
                     await Clients.User(Context.UserIdentifier).SendAsync("ReceiveMessage", user, message);
                 await Clients.User(to).SendAsync("ReceiveMessage", user, message);
+                return;
             }
-            else if (!string.IsNullOrEmpty(roomName))
+            if(!string.IsNullOrEmpty(roomName))
             {
                 await Clients.Group(roomName).SendAsync("ReceiveMessage", user, message);
+                return;
             }
-            else if (!string.IsNullOrEmpty(roomName) && !string.IsNullOrEmpty(to))
-            {
-                await Clients.All.SendAsync("ReceiveMessage", user, message);
-            }
-            /*if (!string.IsNullOrEmpty(roomName) && string.IsNullOrEmpty(to))
-            {
-                await Clients.Group(roomName).SendAsync("ReceiveMessage", user, message);
-            }
-            else if(!string.IsNullOrEmpty(to) && !string.IsNullOrEmpty(roomName))
-            {
-                if (Context.UserIdentifier != to)
-                    await Clients.User(Context.UserIdentifier).SendAsync("ReceiveMessage", user, message);
-                await Clients.User(to).SendAsync("ReceiveMessage", user, message);
-            }*/
-
-            //await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
 
         }
         public override async Task OnConnectedAsync()
@@ -72,6 +47,20 @@ namespace Messenger.Hubs
             await db.SaveChangesAsync();
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
             await Clients.All.SendAsync("Notify", $"{username} disconnected from {roomName} room");
+        }
+        public async Task AddToGroups()
+        {
+            foreach (var group in db.Groups.Include(x => x.Users))
+            {
+                foreach(var u in group.Users)
+                {
+                    if(u.Nickname == Context.UserIdentifier)
+                    {
+                        await Groups.AddToGroupAsync(Context.ConnectionId, group.Name);
+                    }
+                }
+                
+            }
         }
     }
 }
--- Messenger/wwwroot/js/chat.js
+++ Messenger/wwwroot/js/chat.js
@@ -2,40 +2,29 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/Chat").build();
 
-//Disable the send button until connection is established.
 document.getElementById("sendButton").disabled = true;
 
-connection.on("ReceiveMessage", function (user, message, to) {
+connection.on("ReceiveMessage", function (user, message) {
     var li = document.createElement("li");
     document.getElementById("messagesList").appendChild(li);
-    // We can assign user-supplied strings to an element's textContent because it
-    // is not interpreted as markup. If you're assigning in any other way, you 
-    // should be aware of possible script injection concerns.
     li.textContent = `${user} says ${message}`;
 });
 
 connection.on("Notify", function (message) {
 
-    // создает элемент <p> для сообщения пользователя
     let li = document.createElement("li");
     document.getElementById("messagesList").appendChild(li);
 
     li.textContent = `${message}`;
 
 });
-/*connection.on("ReceiveMessagePrivate", function (user, message, to) {
-    var li = document.createElement("li");
-    document.getElementById("messagesList").appendChild(li);
-    // We can assign user-supplied strings to an element's textContent because it
-    // is not interpreted as markup. If you're assigning in any other way, you 
-    // should be aware of possible script injection concerns.
-    li.textContent = `${user} says ${message} to ${to}`;
-});*/
 
 connection.start().then(function () {
     document.getElementById("sendButton").disabled = false;
+    connection.invoke("AddToGroups");
 }).catch(function (err) {
     return console.error(err.toString());
+    
 });
 
 document.getElementById("connectButton").addEventListener("click", function (e) {
@@ -62,3 +51,25 @@ document.getElementById("sendButton").addEventListener("click", function (event)
     });
     event.preventDefault();
 });
+
+/*document.getElementById("sendPrivateButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    var rec = document.getElementById("receiver").value;
+    var userGroup = document.getElementById("userGroup").value;
+    connection.invoke("SendPrivateMessage", user, message, rec, userGroup).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+document.getElementById("sendGroupButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    var rec = document.getElementById("receiver").value;
+    var userGroup = document.getElementById("userGroup").value;
+    connection.invoke("SendMessageGroup", user, message, rec, userGroup).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});*/
